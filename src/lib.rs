ba6c526452bd6d1bca0b63d15f17a2d3b27a2841
Ryan Ward <ryan.ward@praetorian-aeronautics.com>
2026-01-22 08:07:05 +1030

    feat: added `async_executor` macro

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,17 @@ pub fn async_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
     output.into()
 }
+
+/// Generates the appropriate async main executor attribute based on feature flags:
+/// - If "tokio" feature is enabled: generates `#[tokio::main]`
+/// - If "futures" feature is enabled: generates `#[apply(main!)]`
+#[proc_macro_attribute]
+pub fn async_executor(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+    let output = quote! {
+        #[cfg_attr(feature = "tokio", tokio::main)]
+        #[cfg_attr(all(any(feature = "futures"), not(feature = "tokio")), macro_rules_attribute::apply(smol_macros::main!))]
+        #input
+    };
+    output.into()
+}
